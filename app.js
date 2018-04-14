673308d7e7184987181771cfe3a4cd6ce172bca7
ihoronir <20489316+ihoronir@users.noreply.github.com>
2018-04-14 19:03:52 +0900

    refactor

--- app.js
+++ app.js
@@ -1,102 +1,108 @@
 /*
- * モジュールの読み込み
+ * app.js メインアプリケーションプログラム
  */
-// エラーつくるやつ
-var createError = require('http-errors');
-// express
-var express = require('express');
-// パスユーティリティ
-var path = require('path');
-// クッキー使えるようにするやつ
-var cookieParser = require('cookie-parser');
-// body parser
-var bodyParser = require('body-parser');
-// ログ残すやつ
-var logger = require('morgan');
-// Passport
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-// session
-var session = require('express-session');
 
-/**
- * 認証
- */
-passport.use(new LocalStrategy(function(username, password, done){
+
+// --------------- モジュールの読み込み
+
+var createError   = require('http-errors');             // エラー作成
+var path          = require('path');                    // パスユーティリティ
+
+var express       = require('express');                 // express 本体
+var cookieParser  = require('cookie-parser');           // express クッキー
+var bodyParser    = require('body-parser');             // express body parser
+var logger        = require('morgan');                  // express logger
+var session       = require('express-session');         // express session
+
+var passport      = require('passport');                // passport 本体
+var LocalStrategy = require('passport-local').Strategy; // passport ローカルストラテジー
+
+
+
+// --------------- passport の処理
+
+// ローカルストラテジーを使用
+passport.use(new LocalStrategy(function(username, password, done) {
+  // 本来ここには認証処理
   return done(null, 'shioleap');
 }));
 
-// 認証ずみ出なかったら login へリダイレクト
-function isAuthenticated(req, res, next){
+// 未認証だったらリダイレクト
+function isAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {  // 認証済
     return next();
   }
   else {  // 認証されていない
-    res.redirect('/login');  // ログイン画面に遷移
+    res.redirect('/login');  // ログイン画面にリダイレクト
   }
 }
 
-// シリアライズ
+// シリアライズ済みのユーザー
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
+// シリアライズされていないユーザー
 passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
-/*
- * ルーターモジュールの読み込み
- */
-var indexRouter = require('./routes/index');
-var loginRouter = require('./routes/login');
+
+
+// --------------- router モジュールを読み込み
+
+var indexRouter = require('./routes/index'); // / (index)
+var loginRouter = require('./routes/login'); // /login
 //var usersRouter = require('./routes/users');
 
 
-// アプリケーション作成
+
+// --------------- アプリケーション作成
+
 var app = express();
-// view engine setup
+
 // テンプレートフォルダを指定
 app.set('views', path.join(__dirname, 'views'));
+
 // テンプレートエンジンを指定
 app.set('view engine', 'jade');
 
-/*
- * ミドルウェア
- */
 
-// ログを表示
-app.use(logger('dev'));
 
-// いるやつ
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// --------------- ミドルウェア
 
-// cookieParser クッキーを使えるようにする
-app.use(cookieParser());
+app.use(logger('dev')); // ログを表示
 
-// passport
-app.use(passport.initialize());
+app.use(express.json());                          // express.json
+app.use(express.urlencoded({ extended: false })); // urlencoded
+app.use(cookieParser());                          // cookieParser
 
-//static ファイル
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // 静的リソース
+
+app.use(passport.initialize()); // passport
+
 
 // ルーティング
-app.use('/', indexRouter);
-app.use('/login', loginRouter);
+
+app.use('/', indexRouter);      // / (index)
+app.use('/login', loginRouter); // /login
+app.post(                       // /login (post)
+  '/login', 
+  passport.authenticate('local'),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
 //app.use('/users', usersRouter);
-app.post('/login', passport.authenticate('local'), function(req, res){
-  res.redirect('/');
-})
 
 
-// catch 404 and forward to error handler
-// 404エラー
+// 404 エラーハンドラ
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-// エラーハンドラ
+
+// 404 以外のエラーハンドラ
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
