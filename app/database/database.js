c7c251bf6eff78597febb55d4cfaf7725bd86082
ihoronir <20489316+ihoronir@users.noreply.github.com>
2018-04-21 16:03:58 +0900

    connect database

--- app/database/database.js
+++ app/database/database.js
@@ -7,14 +7,60 @@ module.exports = function(config) {
     config.mariadb.user,        // ユーザー
     config.mariadb.password,    // パスワード
     { dialect: 'mysql',         // mysql
-      operatorsAliases: false } // operatorAliase は今の所使わない
+      operatorsAliases: false   // operatorAliase は今の所使わない
+      //logging: false,
+    }
   );
 
   
   sequelize.define('users', {
+    // id
     id: {
-      type: 'BINARY(16)',
-      primaryKey: true
+      type: Sequelize.INTEGER.UNSIGNED, // data type
+      field: 'id',                      // field
+      autoIncrement: true,              // auto increment
+      primaryKey: true,                 // primary
+      allowNull: false                  // not null
+    },
+
+    // name
+    name: {
+      type: Sequelize.CHAR(15),
+      field: 'name',
+      unique: true, // unique
+      allowNull: false
+    },
+
+    // screenName
+    screenName: {
+      type: Sequelize.STRING(50),
+      field: 'screen_name',
+      allowNull: false
+    },
+
+    // email
+    email: {
+      type: Sequelize.STRING(256),
+      field: 'email',
+      allowNull: false
+    },
+
+    // password
+    password: {
+      type: Sequelize.CHAR(64),
+      field: 'password',
+      allowNull: false
     }
+  }, {
+    // createdAt, updatedAt
+    timestamps: true,
+
+    // テーブルネーム
+    tableName: 'users',
+
+    // underscore
+    underscored: true,
   });
+
+  sequelize.sync({force: true});
 };
