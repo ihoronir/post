095e73a14b6eaa538ce8bfc45d04dce660d07da5
ihoronir <20489316+ihoronir@users.noreply.github.com>
2018-04-23 21:36:25 +0900

    update

--- app/passport/local.js
+++ app/passport/local.js
@@ -1,5 +1,10 @@
 var LocalStrategy = require('passport-local').Strategy;
 
+var User = require('../../database/database').user;
+
+var encrypt = require('../../utils/encrypt');
+var saltgen = require('../../utils/salt');
+
 module.exports = new LocalStrategy(function(username, password, done) {
   // 本来ここには認証処理
   return done(null, 'shioleap');
