25f1dcd47a0dcb776e13d00dfcda0742543f3cbc
ihoronir <20489316+ihoronir@users.noreply.github.com>
2018-04-23 23:10:14 +0900

    comment out

--- app/passport/local.js
+++ app/passport/local.js
@@ -1,9 +1,9 @@
 var LocalStrategy = require('passport-local').Strategy;
 
-var User = require('../../database/database').user;
+//var User = require('../../database/database').user;
 
-var encrypt = require('../../utils/encrypt');
-var saltgen = require('../../utils/salt');
+//var encrypt = require('../../utils/encrypt');
+//var saltgen = require('../../utils/salt');
 
 module.exports = new LocalStrategy(function(username, password, done) {
   // 本来ここには認証処理
