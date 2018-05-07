774f7cc14f5092c031e3bfde032bd59eea332506
ihoronir <20489316+ihoronir@users.noreply.github.com>
2018-05-07 23:44:22 +0900

    user <- connect -> database

--- app/routes/pages/user.js
+++ app/routes/pages/user.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('user', {
-    title: 'Express',
-    authenticated: req.isAuthenticated()
-  });
-});
+var User = require('../../database/database').user;
 
 router.get('/:name', function(req, res, next) {
-  res.send(req.params.name);
+  User.findOne({
+    where: {
+      name: req.params.name
+    }
+  }).then(function(user) {
+    res.send(user.screenName);
+  }).catch(function(err) {
+    next(err);
+  })
 })
 
 module.exports = router;
