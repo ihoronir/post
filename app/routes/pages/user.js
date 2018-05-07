cef1d1ffbd02c96c4f5ff556a5fe55930a5e3733
ihoronir <20489316+ihoronir@users.noreply.github.com>
2018-05-07 23:56:11 +0900

    add user view

--- app/routes/pages/user.js
+++ app/routes/pages/user.js
@@ -3,13 +3,17 @@ var router = express.Router();
 
 var User = require('../../database/database').user;
 
+var render = require('../render');
+
 router.get('/:name', function(req, res, next) {
   User.findOne({
     where: {
       name: req.params.name
     }
   }).then(function(user) {
-    res.send(user.screenName);
+    render('user', req, res, {
+      profile: user
+    });
   }).catch(function(err) {
     next(err);
   })
