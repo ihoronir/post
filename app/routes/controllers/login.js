72e88fe5c4306954b0cf284ce799dba70ebdce42
ihoronir <20489316+ihoronir@users.noreply.github.com>
2018-05-24 23:57:57 +0900

    small cange

--- app/routes/controllers/login.js
+++ app/routes/controllers/login.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var passport = require('passport');
 
+/*
 router.post('/',
   passport.authenticate('local', {
     // successRedirect: '/',
@@ -11,6 +12,16 @@ router.post('/',
   }),
   function(req, res) {
     res.redirect(decodeURIComponent(req.body.redirect_to));
+});
+*/
+
+router.post('/', function(req, res, next) {
+  var redirectURL = decodeURIComponent(req.body.redirect_to) || '/';
+  passport.authenticate('local', {
+    successRedirect: redirectURL,
+    failureRedirect: '/login',
+    failureFlash: true
+  })(req, res, next);
 });
 
 module.exports = router;
