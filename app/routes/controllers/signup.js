9b50442afadf05ecf0f3d140f1d92b43fb61c82d
ihoronir <20489316+ihoronir@users.noreply.github.com>
2018-06-10 17:57:35 +0900

    separate validation

--- app/routes/controllers/signup.js
+++ app/routes/controllers/signup.js
@@ -1,70 +1,65 @@
 const express = require('express');
+const router  = express.Router();
 
-/*
- * パスワードハッシュ計算方法
- * 
- * 1. 現在時刻（ミリ秒）をハッシュにかける
- * 2. 1で生成したものをソルトにしてパスワードをハッシュにかける
- * 3. ソルトとハッシュを両方DBに保存しとく
- * 
- */
-
-const router = express.Router();
-
-const User = require('../../database/database').user;
-
-const encrypt = require('../../utils/encrypt');
-const saltgen = require('../../utils/salt');
+const User       = require('../../database/database').user;
+const validation = require('../../utils/validation');
+const encrypt    = require('../../utils/encrypt');
+const saltgen    = require('../../utils/salt');
 
 router.post('/', function(req, res, next) {
 
-  // validation check
-  let errFlag = false;
-  if (!(/^[0-9A-Za-z]+$/.test(req.body.name)) || !req.body.name) {
-    req.flash('validationErrName', '指定されている形式でユーザー ID を入力してください。');
-    errFlag = true;
-  }
-  if (req.body.screen_name === '' || !req.body.screen_name) {
-    req.flash('validationErrScreenName', 'ユーザー名を入力してください。');
-    errFlag = true;
-  }
-  if (!(/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/.test(req.body.email)) || !req.body.email) {
-    req.flash('validationErrEmail', 'メールアドレスを入力してください。');
-    errFlag = true;
-  }
-  if (!(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}/.test(req.body.password)) || !req.body.password) {
-    req.flash('validationErrPassword', '小文字、大文字、数字をそれぞれ含む8文字以上のパスワードを入力してください。');
-    errFlag = true;
-  }
-  if (req.body.password !== req.body.password_confirmation || !req.body.password_confirmation) {
-    req.flash('validationErrPasswordConfirmation', '同じパスワードを入力してください。');
-  }
-  // errRedirect
-  if (errFlag) {
-    return res.redirect('/signup');
-  }
-
-  const salt = saltgen();
-  const password = encrypt(req.body.password, salt);
-  User.build({
-    name: req.body.name,
-    screenName: req.body.screen_name,
-    email: req.body.email,
-    password: password, // パスワード
-    passwordSalt: salt, // ソルト
-    description: ''     // プロフィール
-  }).save().then(function() {
-
-    res.redirect('/login');
-
-  }).catch(function(err) {
-
-    if (err.name === 'SequelizeUniqueConstraintError') {
-      req.flash('validationErrName', 'ユーザーID ' + req.body.name + ' はすでに使用されています。');
-      res.redirect('/signup');
-    } else {
-      next(err);
+  validation(req, [
+    {
+      text: req.body.name,
+      pattern: /^[0-9A-Za-z]+$/,
+      flash: 'validationErrName',
+      flashMessage: '指定されている形式でユーザー ID を入力してください。'
+    }, {
+      text: req.body.screen_name,
+      pattern: /.*/,
+      flash: 'validationErrScreenName',
+      flashMessage: 'ユーザー名を入力してください。'
+    }, {
+      text: req.body.email,
+      pattern: /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/,
+      flash: 'validationErrEmail',
+      flashMessage: 'メールアドレスを入力してください。'
+    }, {
+      text: req.body.password,
+      pattern: /(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}/,
+      flash: 'validationErrPassword',
+      flashMessage: '小文字、大文字、数字をそれぞれ含む8文字以上のパスワードを入力してください。'
+    }, {
+      text: req.body.password_confirmation,
+      pattern: new RegExp(req.body.password),
+      flash: 'validationErrPasswordConfirmation',
+      flashMessage: '同じパスワードを入力してください。'
     }
+  ]).then(() => {
+    const salt = saltgen();
+    const password = encrypt(req.body.password, salt);
+    User.build({
+      name: req.body.name,
+      screenName: req.body.screen_name,
+      email: req.body.email,
+      password: password, // パスワード
+      passwordSalt: salt, // ソルト
+      description: ''     // プロフィール
+    }).save().then(() => {
+
+      res.redirect('/login');
+
+    }).catch((err) => {
+
+      if (err.name === 'SequelizeUniqueConstraintError') {
+        req.flash('validationErrName', 'ユーザーID ' + req.body.name + ' はすでに使用されています。');
+        res.redirect('/signup');
+      } else {
+        next(err);
+      }
+    });
+  }).catch(() => {
+    return res.redirect('/signup');
   });
 });
 
---
+++ app/utils/validation.js
@@ -0,0 +1,16 @@
+module.exports = (req, input) => {
+  let errFlag = false;
+  for (let i = 0 , length = input.length; i < length; i ++) {
+    if (!(input[i].pattern.test(input[i].text)) || !input[i].text) {
+      req.flash(input[i].flash, input[i].flashMessage);
+      errFlag = true;
+    }
+  }
+  return new Promise((resolve, reject) => {
+    if (errFlag) {
+      reject();
+    } else {
+      resolve();
+    }
+  });
+}
