0416b68a6f957856896c0d2b76099a86a46250e2
ihoronir <20489316+ihoronir@users.noreply.github.com>
2018-08-18 13:33:21 +0900

    fs-routing

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,44 +1,8 @@
 'use strict';
 
-const fs = require('fs');
 const path = require('path');
 const csrf = require('csurf');
-const express = require('express');
-
-const walk = (p, fileCallback) => {
-  const files = fs.readdirSync(p);
-
-  files.forEach(f => {
-    const fp = path.join(p, f);
-    if (fs.statSync(fp).isDirectory()) {
-      walk(fp, fileCallback);
-    } else {
-      fileCallback(fp);
-    }
-  });
-};
-
-const fsRouter = (method, dir) => {
-  const router = express.Router();
-  walk(dir, filePath => {
-    const url =
-      '/' +
-      path
-        .relative(dir, filePath)
-        .slice(0, -3)
-        .replace(/_/g, ':')
-        .replace(/index/g, '')
-        .replace(/\/$/, '');
-
-    const routes = require(filePath);
-    if (routes instanceof Array) {
-      router[method](url, ...routes);
-    } else {
-      router[method](url, routes);
-    }
-  });
-  return router;
-};
+const fsRouter = require('fs-routing');
 
 module.exports = app => {
   app.use(fsRouter('post', path.join(__dirname, './multipartPost/')));
