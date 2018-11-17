3a2e34767499fa1394868fe7071da53b69011658
ihoronir <20489316+ihoronir@users.noreply.github.com>
2018-11-17 15:00:00 +0900

    update packages

--- config/production.js
+++ config/production.js
@@ -4,6 +4,8 @@ const path = require('path');
 const Config = require('./config');
 const config = new Config();
 
-config.addFromPath('mariadb', path.join(__dirname, './environment/prodMariaDB')).addFromPath('secret', path.join(__dirname, './environment/prodSecrets'));
+config
+  .addFromPath('mariadb', path.join(__dirname, './environment/prodMariaDB'))
+  .addFromPath('secret', path.join(__dirname, './environment/prodSecrets'));
 
 module.exports = config;
