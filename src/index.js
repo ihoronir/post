d7ca54e6746d97622839ec49bbc615fc0214885c
ihoronir <20489316+ihoronir@users.noreply.github.com>
2018-12-23 18:19:27 +0900

    flocss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // import 文を使ってstyle.cssファイルを読み込む。
-import './stylesheets/style.sass';
+import './stylesheets/app.sass';
 
 // import 文を使って sub.js ファイルを読み込む。
 import { hello } from './scripts/sub';
